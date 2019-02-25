f94a6a9c3806a1e181ec9ef548c23f3cf026d405
TotallyNotHuman <tnhthereal@gmail.com>
2019-02-25 11:45:28 -0500

    Uncomment star stats

--- src/star.h
+++ src/star.h
@@ -2,10 +2,10 @@
 #define STAR_H
 
 typedef struct star {
-    /*double mass; // mass of star in M_sol
+    double mass; // mass of star in M_sol
     double radius; // radius of star in R_sol
     double luminosity; // luminosity of star in L_sol
-    unsigned short temp; // temperature of star in K*/ // TODO: do research on star statistic ranges
+    unsigned short temp; // temperature of star in K
     char spectype; // spectral class of star
     unsigned short subdiv; // digit subdivision of star
     char* lumtype; // luminosity class of star
